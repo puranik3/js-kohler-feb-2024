63d863e4a0bce41df1ee4d2812fca0bce469dfa5
Prashanth Puranik <puranik@digdeeper.in>
2024-02-13 11:21:43 +0530

    exercise continued

--- exercises/03-callback-hell.js
+++ exercises/03-callback-hell.js
@@ -11,5 +11,7 @@ sum( 12, 13, function( result1 ) {
     console.log( 'result1 = ', result1 );
 
     // Use sum() to find the sum of result1 and 14, and log it
-    // your code...
+    sum( result1, 14, function( result2 ) {
+        console.log( 'result2 = ', result2 );
+    })
 });
