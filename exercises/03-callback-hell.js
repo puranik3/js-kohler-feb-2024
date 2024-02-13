6270cf5dfa26712cce3e113f8453633d84bceb9e
Prashanth Puranik <puranik@digdeeper.in>
2024-02-13 11:09:28 +0530

    exercise

---
+++ exercises/03-callback-hell.js
@@ -0,0 +1,15 @@
+function sum( x, y, callback ) {
+    setTimeout(
+        function() {
+            callback( x + y );
+        },
+        3000
+    );
+}
+
+sum( 12, 13, function( result1 ) {
+    console.log( 'result1 = ', result1 );
+
+    // Use sum() to find the sum of result1 and 14, and log it
+    // your code...
+});
