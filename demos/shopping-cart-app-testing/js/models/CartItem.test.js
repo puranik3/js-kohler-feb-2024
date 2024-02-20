5d071f58ee6126871852bc4f192d4cba83c70b13
Prashanth Puranik <puranik@digdeeper.in>
2024-02-20 09:57:43 +0530

    CartItem tests

--- demos/shopping-cart-app-testing/js/models/CartItem.test.js
+++ demos/shopping-cart-app-testing/js/models/CartItem.test.js
@@ -1,13 +1,23 @@
 import CartItem from './CartItem';
 import products from '../../mocks/data/products.json';
 
-test('CartItem constructor creates an object with product and quantity that are passed', () => {
-    // arranging and acting
-    const item = new CartItem( products[0], 2 );
+describe('CartItem', () => {
+    let item;
 
-    // assertions
-    expect( item ).toEqual({
-        product: products[0],
-        quantity: 2
+    beforeEach(() => {
+        // arranging and acting
+        item = new CartItem( products[0], 2 );
+    });
+
+    test('constructor creates an object with product and quantity that are passed', () => {
+        // assertions
+        expect( item ).toEqual({
+            product: products[0],
+            quantity: 2
+        });
+    });
+    
+    test('getTotal() returns the correct total for an item', () => {
+        expect( item.getTotal() ).toEqual( products[0]['Retail Price'] * 2 );
     });
 });
